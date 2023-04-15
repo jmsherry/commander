f82cd9392a0a5545a4f1c5cb13e3748975a54120
James Sherry <james.m.sherry@googlemail.com>
2023-04-15 07:48:45 +0100

    task list done

--- todo-list.js
+++ todo-list.js
@@ -3,64 +3,29 @@ import Listr from "listr";
 
 const tasks = new Listr([
   {
-    title: "Git",
-    task: () => {
-      return new Listr(
-        [
-          {
-            title: "Checking git status",
-            task: async () => {
-              const { stdout } = await execa("git", ["status", "--porcelain"]);
-
-              if (stdout !== "") {
-                throw new Error(
-                  "Unclean working tree. Commit or stash changes first."
-                );
-              }
-            },
-          },
-          {
-            title: "Checking remote history",
-            task: async () => {
-              const { stdout } = await execa("git", [
-                "rev-list",
-                "--count",
-                "--left-only",
-                "@{u}...HEAD",
-              ]);
-
-              if (stdout !== "0") {
-                throw new Error("Remote history differ. Please pull changes.");
-              }
-            },
-          },
-        ],
-        { concurrent: true }
-      );
+    title: "Check node version",
+    task: async () => {
+      const { stdout } = await execa("node", ["-v"]);
+      console.log(`Node version: ${stdout}`);
     },
   },
-  // {
-  // 	title: 'Install package dependencies with Yarn',
-  // 	task: (ctx, task) => execa('yarn')
-  // 		.catch(() => {
-  // 			ctx.yarn = false;
-
-  // 			task.skip('Yarn not available, install it via `npm install -g yarn`');
-  // 		})
-  // },
   {
-    title: "Install package dependencies with npm",
-    enabled: (ctx) => ctx.yarn === false,
-    task: () => execa("npm", ["install"]),
+    title: "Check npm version",
+    task: async () => {
+      const { stdout } = await execa("npm", ["-v"]);
+      console.log(`NPM version: ${stdout}`);
+    },
   },
   {
-    title: "Run tests",
-    task: () => execa("npm", ["test"]),
+    title: "Success",
+    task: () => new Promise((resolve, reject) => {
+      setTimeout(resolve('bar'), 2000);
+    }),
   },
-  // {
-  // 	title: 'Publish package',
-  // 	task: () => execa('npm', ['publish'])
-  // }
+  {
+    title: "Failure",
+    task: () => Promise.reject(new Error("Bar")),
+  },
 ]);
 
 tasks.run().catch((err) => {
